3377a2d3f26b30dafe9b65d549344932ea87b107
NigelJohn <90619326+NigelJohn@users.noreply.github.com>
2021-09-26 17:08:44 +0530

    Add files via upload
    isme html ka jo ml5 ka link hai woh unresponsive aa raha hai
    without uske I can't go ahead agar kuch soln mila toh update karo grp pe

---
+++ Physio/physio.js
@@ -0,0 +1,41 @@
+let poseNet;
+
+function setup() {
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
+function modelLoaded(params) {
+    console.log('poseNet ready');
+}
+// Redirection
+function contactRedirect() {
+    window.location.href = "contact.html";
+}
+function physioRedirect() {
+    window.location.href = "physio.html";
+}
+
+// Gaining live camera access
+var stop = function () {
+    var stream = video.srcObject;
+    var tracks = stream.getTracks();
+    for (var i =0; i < tracks.length; i++) {
+        var track = tracks[i];
+        track.stop();
+    }
+    video.srcObject = null;
+} 
+var start = function () {
+    var video = document.getElementById('video'), vendorUrl = window.URL || window.webkitURL;
+    if (navigator.mediaDevices.getUserMedia){
+        navigator.mediaDevices.getUserMedia({video: true})
+            .then(function (stream) {
+                video.srcObject = stream;
+            }).catch(function (error) {
+                console.log("Something went wrong!");
+            });
+    }
+}
+
+// $(function () {
+//     start();
+// });
